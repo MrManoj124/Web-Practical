daa6af18e723ea459f0f352e33271d367c1b2d20
MrManoj124 <manojmanorooban@gmail.com>
2025-07-02 17:52:37 +0530

    Created a studentRoute.js file

---
+++ routes/studentRoute.js
@@ -0,0 +1,18 @@
+//const mongoose = require('mongoose')
+const express=require('express');
+const router=express.Router();
+const Student=require("../models/studentRoute");
+
+router.get('/', async (req,res)=>{
+    try{
+    const students= await Student.find();
+    res.json(students);
+    }
+    catch(error){
+        //console.error(error)
+        res.status(500).json({ message: err.message})
+    }
+})
+
+
+module.exports=router
