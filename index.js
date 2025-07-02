9e232cca60a7d9c3f78b1e7b6a661346fde2ecf8
MrManoj124 <manojmanorooban@gmail.com>
2025-07-02 19:00:53 +0530

    modify index.js file

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port=3002;
 const mongoose=require('mongoose');
 
 const studentsRoute= require('./routes/studentRoute')
-//const eventrot= require('./models/events')
-//const certificaterot= require('./models/certificates')
+const eventRoute= require('./routes/eventRoute')
+const certificateRoute= require('./routes/certificateRoute')
 
 Cert.use(express.json())
 Cert.use('/students',studentsRoute)
-//Cert.use('/events',eventrot)
-//Cert.use('/certificates',certificaterot)
+Cert.use('/events',eventRoute)
+Cert.use('/certificates',certificateRoute)
 
 
 mongoose.connect('mongodb://localhost:27017/Awarding').then(()=>{
