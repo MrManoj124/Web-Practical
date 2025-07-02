38d678e7be6828eae06b57ef3048f5017941e628
MrManoj124 <manojmanorooban@gmail.com>
2025-07-02 18:34:34 +0530

    Update studentRoute.js file

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,7 +1,7 @@
 //const mongoose = require('mongoose')
 const express=require('express');
 const router=express.Router();
-const Student=require("../models/student");
+const Student=require("../models/students");
 
 router.get('/', async (req,res)=>{
     try{
@@ -14,5 +14,15 @@ router.get('/', async (req,res)=>{
     }
 })
 
+router.get('/degree', async (req,res)=>{
+    try{
+    const students= await Student.find();
+    res.json(students);
+    }
+    catch(error){
+        //console.error(error)
+        res.status(500).json({ message: err.message})
+    }
+})
 
 module.exports=router
